4bba7a73c37885e767b90ab2d0068ea284fee97b
santi gonzalo <sgonzalo84@gmail.com>
2020-03-29 19:34:09 +0200

    Añadir contenido, rutas y componentes secundarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {CoverComponent} from './cover/cover.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
+import {OfferComponent} from './offer/offer.component';
+import {TargetComponent} from './target/target.component';
+import {ValuesComponent} from './values/values.component';
+import {GalleryComponent} from './gallery/gallery.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'oferta', component: OfferComponent},
+  {path: 'publico', component: TargetComponent},
+  {path: 'valores', component: ValuesComponent},
+  {path: 'galeria', component: GalleryComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoverComponent,
+    HomeComponent,
+    FooterComponent,
+    OfferComponent,
+    TargetComponent,
+    ValuesComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
